31681a34b994974021952968ebdef5f12dfdd039
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2023-01-10 17:39:37 +0800

    configure

--- 202209-1/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/rainchen/CodingArea/ccf-csp-problemset/202209-1
-# Build directory: /Users/rainchen/CodingArea/ccf-csp-problemset/202209-1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- 202209-2/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/rainchen/CodingArea/ccf-csp-problemset/202209-2
-# Build directory: /Users/rainchen/CodingArea/ccf-csp-problemset/202209-2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
