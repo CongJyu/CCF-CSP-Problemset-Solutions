c85786aa0fee9aea0ee54d5ec4d729e182d8f568
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2023-01-11 23:25:26 +0800

    add 202209-3

---
+++ 202209-3/main.cpp
@@ -0,0 +1,19 @@
+// ccf csp 202209-3
+
+#include <iostream>
+
+using namespace std;
+
+struct Data {
+    int day;  // 日期
+    int user;  // 用戶
+    int region;  // 地區
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n{};
+    cin >> n;  // 輸入天數
+    
+    return 0;
+}
