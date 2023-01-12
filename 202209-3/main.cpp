632e7c9e9799cbe8176890bb635297eb7590b519
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2023-01-12 11:38:55 +0800

    Change clang-format, add a function.

--- 202209-3/main.cpp
+++ 202209-3/main.cpp
@@ -1,6 +1,8 @@
 // ccf csp 202209-3
 
 #include <iostream>
+#include <vector>
+#include <map>
 
 using namespace std;
 
@@ -9,11 +11,33 @@ struct Data {
     int user;  // 用戶
     int region;  // 地區
 };
+vector<Data> roaming_data;  // 漫遊消息
+map<int, pair<int, int> > region_risk_last_time;  // 每個地區嘅風險持續時間
+map<int, bool> is_risk_region;  // 每個地區是否被設為風險區
+
+void set_risk_region(int this_region, int this_day) {
+    // 將 this_region 設定為 this_day 風險區
+    if (!is_risk_region.at(this_region)) {
+        region_risk_last_time.at(this_region) = {this_day, this_day + 6};
+    } else {
+        if (this_day <= region_risk_last_time.at(this_region).second + 1) {
+            region_risk_last_time.at(this_region).second = this_day + 6;  // 可以連起來
+        } else {
+            region_risk_last_time.at(this_region) = {this_day, this_day + 6};
+        }
+    }
+    is_risk_region.at(this_region) = true;
+}
 
 int main() {
     ios::sync_with_stdio(false);
     int n{};
     cin >> n;  // 輸入天數
-    
+    int ri{}, mi{};  // 地區數目 and 訊息數目
+    for (int i{}; i < n; ++i) {
+        cin >> ri >> mi;
+        for (int j{}; j < ri; ++j) {
+        }
+    }
     return 0;
 }
