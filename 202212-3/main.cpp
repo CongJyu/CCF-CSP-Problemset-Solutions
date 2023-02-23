8c331489e0613993f0fe63ef59d37cc4753a4060
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2023-02-24 00:00:51 +0800

    add 202212-3

---
+++ 202212-3/main.cpp
@@ -0,0 +1,27 @@
+// ccf csp 202212-3
+
+#include <iostream>
+
+using namespace std;
+
+int q_mat[1001][1001];
+int m_mat[8][8] = {0};
+int scanned_data[10001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    for (int i{}; i < 8; ++i) {
+        for (int j{}; j < 8; ++j) {
+            cin >> q_mat[i][j];
+        }
+    }
+    int n{};  // 數據個數
+    cin >> n;
+    int T{};  // 任務
+    cin >> T;
+    for (int i{}; i < n; ++i) {
+        cin >> scanned_data[i];
+    }
+    // 仲未完成，聽日繼續
+    return 0;
+}
